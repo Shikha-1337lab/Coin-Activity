89075b9dcbeb0670cf40bf0bcffedf3e7f6e88bb
Shikha Singh <shikha.singh9115@gmail.com>
2018-07-24 11:49:08 -0400

    Added the missing links

--- coinactivity/urls.py
+++ coinactivity/urls.py
@@ -7,6 +7,6 @@ from coins import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     # path('coins/', include('products.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
